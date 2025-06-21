7a01c6f3292b8af57fc0fb35564b7394eb6c24ae
Vinodh Kumar Markapuram <GreenHex@gmail.com>
2025-06-21 21:17:37 +0530

    v1.2

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _s2 = s1.clone(); // forward signals to bl_pwm()
 
     let (c_s1, r_s1) = unbounded::<CryptoResult>(); // crypto_thd()
+
     let crypto_result = Arc::new(Mutex::new(CryptoResult::new_empty())); // crypto_thd()
     let crypto_result1 = crypto_result.clone(); // http_server()
     let crypto_result2 = crypto_result.clone(); // usb_thd()
@@ -127,115 +128,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_max_buffer_size(64);
 
     let _ = lcd_setup(&mut l);
-    let mut btc: String = String::from("waiting...");
 
     // MAIN LOOP
     while !term_now.load(Ordering::Relaxed) {
-        l.lcd_set_window(0, 0, IMG_WIDTH, IMG_HEIGHT).unwrap();
-
-        l.img_draw_rect2(0, 0, IMG_WIDTH, 32, WHITE);
-        l.img_draw_string(
-            &((IMG_WIDTH - get_time_str().len() * FONT12.width) / 2),
-            &(8),
-            &(get_time_str()),
-            &FONT12,
-            BLACK,
-            WHITE,
-        );
-
-        l.img_draw_string(
-            &(4),
-            &(42),
-            &("IP Address".to_string()),
-            &FONT8,
-            BLUE2,
-            BLACK,
-        );
-        l.img_draw_string(
-            &((IMG_WIDTH - get_ip().len() * FONT12.width) - 4),
-            &(42 + 24),
-            &(get_ip()),
-            &FONT12,
-            WHITE,
-            BLACK,
-        );
-        l.img_draw_rect2(0, 42 + 24 + 2 + 2 + FONT12.height * 2, IMG_WIDTH, 1, ORANGE);
-
-        let (_, uptime, load, temp) = get_cpu_info();
-        l.img_draw_string(&(4), &(102), &("Uptime".to_string()), &FONT8, BLUE2, BLACK);
-        l.img_draw_string(
-            &((IMG_WIDTH - uptime.len() * FONT12.width) - 4),
-            &(102 + 24),
-            &(uptime),
-            &FONT12,
-            WHITE,
-            BLACK,
-        );
-        l.img_draw_rect2(
-            0,
-            102 + 24 + 2 + 2 + FONT12.height * 2,
-            IMG_WIDTH,
-            1,
-            ORANGE,
-        );
-
-        l.img_draw_string(&(4), &(162), &("Load".to_string()), &FONT8, BLUE2, BLACK);
-        l.img_draw_string(
-            &((IMG_WIDTH / 2 - load.len() * FONT12.width) - 4),
-            &(162 + 24),
-            &(load),
-            &FONT12,
-            WHITE,
-            BLACK,
-        );
-
-        l.img_draw_string(
-            &(IMG_WIDTH / 2 + 6),
-            &(162),
-            &("Temp".to_string()),
-            &FONT8,
-            BLUE2,
-            BLACK,
-        );
-        l.img_draw_string(
-            &(IMG_WIDTH / 2 + (IMG_WIDTH / 2 - temp.len() * FONT12.width) - 4),
-            &(162 + 24),
-            &(temp),
-            &FONT12,
-            WHITE,
-            BLACK,
-        );
-
-        l.img_draw_rect2(
-            IMG_WIDTH / 2,
-            102 + 24 + 2 + 2 + FONT12.height * 2,
-            1,
-            64,
-            ORANGE,
-        );
-
-        l.img_draw_rect2(1, 218, IMG_WIDTH - 2, FONT16.height * 2 + 2 + 2 + 2, ORANGE);
-
-        match r_s1.try_recv() {
-            Ok(crypto_result) => {
-                btc = crypto_result.btc_cmp_str.clone();
-                crypto_result.print();
-            }
-            _ => {}
-        };
-
-        let btc_len: usize = (IMG_WIDTH - btc.clone().len() * FONT16.width) / 2;
-
-        l.img_draw_string(
-            &((IMG_WIDTH - btc.len() * FONT16.width) / 2),
-            &(220 + 4),
-            &btc,
-            &FONT16,
-            BLACK,
-            ORANGE,
-        );
-
-        l.img_draw_image(0, 0, LCD_WIDTH, LCD_HEIGHT);
+        lcd_display_stuff(&mut l, &r_s1);
 
         thread::sleep(Duration::from_secs(SCREEN_UPDATE_INTERVAL_SECS));
 
@@ -315,4 +211,115 @@ fn lcd_setup(l: &mut Lcd) -> Result<(), Box<dyn Error>> {
     l.img_clear(BLACK);
 
     return Ok(());
+}
+
+fn lcd_display_stuff(l: &mut Lcd, r_s1: &crossbeam_channel::Receiver<CryptoResult>) {
+    l.lcd_set_window(0, 0, IMG_WIDTH, IMG_HEIGHT).unwrap();
+
+    l.img_draw_rect2(0, 0, IMG_WIDTH, 32, WHITE);
+    l.img_draw_string(
+        &((IMG_WIDTH - get_time_str().len() * FONT12.width) / 2),
+        &(8),
+        &(get_time_str()),
+        &FONT12,
+        BLACK,
+        WHITE,
+    );
+
+    l.img_draw_string(
+        &(4),
+        &(42),
+        &("IP Address".to_string()),
+        &FONT8,
+        BLUE2,
+        BLACK,
+    );
+    l.img_draw_string(
+        &((IMG_WIDTH - get_ip().len() * FONT12.width) - 4),
+        &(42 + 24),
+        &(get_ip()),
+        &FONT12,
+        WHITE,
+        BLACK,
+    );
+    l.img_draw_rect2(0, 42 + 24 + 2 + 2 + FONT12.height * 2, IMG_WIDTH, 1, ORANGE);
+
+    let (_, uptime, load, temp) = get_cpu_info();
+    l.img_draw_string(&(4), &(102), &("Uptime".to_string()), &FONT8, BLUE2, BLACK);
+    l.img_draw_string(
+        &((IMG_WIDTH - uptime.len() * FONT12.width) - 4),
+        &(102 + 24),
+        &(uptime),
+        &FONT12,
+        WHITE,
+        BLACK,
+    );
+    l.img_draw_rect2(
+        0,
+        102 + 24 + 2 + 2 + FONT12.height * 2,
+        IMG_WIDTH,
+        1,
+        ORANGE,
+    );
+
+    l.img_draw_string(&(4), &(162), &("Load".to_string()), &FONT8, BLUE2, BLACK);
+    l.img_draw_string(
+        &((IMG_WIDTH / 2 - load.len() * FONT12.width) - 4),
+        &(162 + 24),
+        &(load),
+        &FONT12,
+        WHITE,
+        BLACK,
+    );
+
+    l.img_draw_string(
+        &(IMG_WIDTH / 2 + 6),
+        &(162),
+        &("Temp".to_string()),
+        &FONT8,
+        BLUE2,
+        BLACK,
+    );
+    l.img_draw_string(
+        &(IMG_WIDTH / 2 + (IMG_WIDTH / 2 - temp.len() * FONT12.width) - 4),
+        &(162 + 24),
+        &(temp),
+        &FONT12,
+        WHITE,
+        BLACK,
+    );
+
+    l.img_draw_rect2(
+        IMG_WIDTH / 2,
+        102 + 24 + 2 + 2 + FONT12.height * 2,
+        1,
+        64,
+        ORANGE,
+    );
+
+    l.img_draw_rect2(1, 218, IMG_WIDTH - 2, FONT16.height * 2 + 2 + 2 + 2, ORANGE);
+
+    let mut btc: String = String::from("waiting...");
+    match r_s1.try_recv() {
+        Ok(crypto_result) => {
+            btc = crypto_result.btc_cmp_str.clone();
+            crypto_result.print();
+        }
+        _ => {}
+    };
+
+    let btc_len: usize = (IMG_WIDTH - btc.clone().len() * FONT16.width) / 2;
+
+    l.img_draw_string(
+        &((IMG_WIDTH - btc.len() * FONT16.width) / 2),
+        &(220 + 4),
+        &btc,
+        &FONT16,
+        BLACK,
+        ORANGE,
+    );
+
+    l.img_draw_image(0, 0, LCD_WIDTH, LCD_HEIGHT);
+
+    return;
 }
